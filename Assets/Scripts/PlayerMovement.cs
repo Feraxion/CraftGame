7bb18577f30a940ea483057144e2b8efe9009820
NagataKyoshi <Nagatakyoshii@gmail.com>
2021-03-10 23:07:31 +0300

    scene

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    protected Joystick joystick;
     // Start is called before the first frame update
     void Start()
     {
-        
+        joystick = FindObjectOfType<Joystick>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3(joystick.Horizontal * 100, rigidbody.velocity.y, joystick.Vertical * 100f);
     }
 }
