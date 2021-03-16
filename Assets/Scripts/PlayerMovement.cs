df269ae5fb6cd67d2fcd5231c51cb937ef1f3af1
NagataKyoshi <Nagatakyoshii@gmail.com>
2021-03-16 04:29:22 +0300

    Joystick - Movement issues solved

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,21 +21,10 @@ public class PlayerMovement : MonoBehaviour
 
         rigidbody.velocity = new Vector3(joystick.Horizontal * 5f, rigidbody.velocity.y, joystick.Vertical * 5f);
 
-        //float inputZ = joystick.Horizontal;
-        //float inputX = -joystick.Vertical;
+        transform.eulerAngles = new Vector3(0,Mathf.Atan2(joystick.Horizontal, joystick.Vertical) * 180 / Mathf.PI,0);
 
-        //Vector3 lookDirection = new Vector3(inputX, 0, inputZ);
-        //Quaternion lookRotation = Quaternion.LookRotation(lookDirection, Vector3.up);
-
-        //float step = rotationSpeed * Time.deltaTime;
-        //transform.rotation = Quaternion.RotateTowards(lookRotation, transform.rotation, step);
-        // that code useful for character rotation
-
-        transform.eulerAngles = new Vector3(0,Mathf.Atan2(joystick.Vertical, joystick.Horizontal) * 180 / Mathf.PI,0);
-        //rigidbody.rotation = rigidbody.rotation * Quaternion.AngleAxis(joystick.Horizontal * turnSpeed, Vector3.up);  // PIVOT ISSUE - AFTER SOLVE IT OPEN THAT CODE
         animator.SetFloat("Speed", Mathf.Abs(joystick.Horizontal));
         
-        
-        //transform.Rotate(new Vector3(transform.position.x, transform.position.y * (joystick.Horizontal * 5f), transform.position.z));
+
     }
 }
