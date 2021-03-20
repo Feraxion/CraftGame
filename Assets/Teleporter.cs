3c9c4ac371c1c434e29a92e65b9df690138ac4be
Feraxion <32463690+Feraxion@users.noreply.github.com>
2021-03-20 18:55:35 +0300

    Teleporter

---
+++ Assets/Teleporter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleporter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        
+        if (other.gameObject.tag == "Player")
+        {
+
+            if (scene.buildIndex == 0)
+            {
+                SceneManager.LoadScene(1);
+            }
+            
+            if (scene.buildIndex == 1)
+            {
+                SceneManager.LoadScene(0);
+            }
+            
+        }
+    }
+}
